7d1f323bf73b2b2de4cc875c9a63a189c66faddd
Jordi Martínez Zambrano <jordi.martinez@sap.com>
2020-02-25 14:16:38 +0100

    lesson 15

---
+++ features/annotations/variables.ts
@@ -0,0 +1,15 @@
+let apples: number = 5;
+const peaches: number = true;
+
+apples = 'six';
+
+let speed: string = 'fast';
+speed = 50;
+
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+nothingMuch = 5;
+
+// built in objects
+let now: Date = new Date();
