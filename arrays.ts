120f99007efb5fe7a79ce0a3d066319acfe593f5
Jordi M. Zambrano <nickschoey@gmail.com>
2020-02-25 20:11:39 +0100

    lesson 30

--- arrays.ts
+++ arrays.ts
@@ -3,3 +3,15 @@ const carMakers = ['ford', 'toyota', 'mercedes'];
 const dates = [new Date(), new Date()];
 
 const carsByMake = [['mustang'], ['corolla'], ['class a']];
+
+// help with inference when extracting values
+const slowCar = carMakers[1];
+const myCar = carMakers.pop();
+
+// prevent incompatible values to be added
+carMakers.push(100);
+
+// help with map and we get all the methods for this
+carMakers.map((car: string): string => car);
+
+// They are flexible
