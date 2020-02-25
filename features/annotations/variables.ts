264ba000c571cffb1cc236fb275e09c5023cd200
Jordi Martínez Zambrano <jordi.martinez@sap.com>
2020-02-25 14:23:36 +0100

    lesson 16

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -13,3 +13,22 @@ nothingMuch = 5;
 
 // built in objects
 let now: Date = new Date();
+
+// Array
+
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, false, true];
+
+// Classes
+
+class Car {}
+
+let car: Car = new Car();
+
+// Object literal
+
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20
+};
