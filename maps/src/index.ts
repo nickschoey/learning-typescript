593088cbad5414dfb2233a774cdf4c90c1c2c1fd
Jordi M. Zambrano <nickschoey@gmail.com>
2020-02-26 16:08:00 +0100

    lesson 64

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,8 +1,10 @@
 /// <reference types="@types/googlemaps" />
 import { CustomMap } from './CustomMap';
 import { User } from './User';
+import { Company } from './Company';
 
 const user = new User();
 const customMap = new CustomMap('map');
-
+const company = new Company();
 customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
