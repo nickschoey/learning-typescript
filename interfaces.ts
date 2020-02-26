dea455caa6d5f8475d24549958332b896beae144
Jordi M. Zambrano <nickschoey@gmail.com>
2020-02-26 08:44:02 +0100

    lesson 38

--- interfaces.ts
+++ interfaces.ts
@@ -1,18 +1,16 @@
-const oldCivic = {
+interface Vehicle {
+  name: string;
+  year: number;
+  broken: boolean;
+}
+
+const oldCivic: Vehicle = {
   name: 'Civic',
   year: 2000,
   broken: true
 };
 
-const printVehicle = ({
-  year,
-  name,
-  broken
-}: {
-  name: string;
-  year: number;
-  broken: boolean;
-}): void => {
+const printVehicle = ({ year, name, broken }: Vehicle): void => {
   console.log(`This ${year} ${name} is ${broken}`);
 };
 
