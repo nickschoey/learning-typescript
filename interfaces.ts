e9671f1fa38b00e52afc61f42faa300f326b7396
Jordi M. Zambrano <nickschoey@gmail.com>
2020-02-26 08:47:12 +0100

    lesson 39

--- interfaces.ts
+++ interfaces.ts
@@ -2,12 +2,16 @@ interface Vehicle {
   name: string;
   year: number;
   broken: boolean;
+  summary(): string;
 }
 
 const oldCivic: Vehicle = {
   name: 'Civic',
   year: 2000,
-  broken: true
+  broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  }
 };
 
 const printVehicle = ({ year, name, broken }: Vehicle): void => {
