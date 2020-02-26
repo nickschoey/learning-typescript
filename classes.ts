9b6f84b29e0b10bbe761a0f1258cb0a0a5fbca5a
Jordi M. Zambrano <nickschoey@gmail.com>
2020-02-26 09:49:07 +0100

    lesson 44

--- classes.ts
+++ classes.ts
@@ -2,8 +2,13 @@ class Vehicle {
   drive(): void {
     console.log('vruum!!');
   }
+
+  honk(): void {
+    console.log('beep!');
+  }
 }
 
+class Car extends Vehicle {}
 const vehicle = new Vehicle();
 
 vehicle.drive();
