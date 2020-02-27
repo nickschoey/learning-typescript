a1ef5e4906b97864ef80fe9ef00c951ca72f0e4d
Jordi M. Zambrano <nickschoey@gmail.com>
2020-02-27 09:00:35 +0100

    lesson 79

--- sorter/src/index.ts
+++ sorter/src/index.ts
@@ -18,17 +18,20 @@ const array = [200, 1, 5, 2, 23, 322, 3, 5, 76, -1, 6, 64];
 // console.log(sort(array));
 
 class Sorter {
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
   sort(): void {
     const { length } = this.collection;
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        const left = array[j];
-        const right = array[j + 1];
+        const left = this.collection[j];
+        const right = this.collection[j + 1];
+        // if this.collection is array of numbers
+
         if (left > right) {
-          array[j] = right;
-          array[j + 1] = left;
+          this.collection[j] = right;
+          this.collection[j + 1] = left;
         }
+        // if collection is string
       }
     }
   }
