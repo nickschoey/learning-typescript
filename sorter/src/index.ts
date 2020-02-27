3306f90ca848d4eee96e4386bf4484eb333f0053
Jordi M. Zambrano <nickschoey@gmail.com>
2020-02-27 08:28:25 +0100

    lesson 76

--- sorter/src/index.ts
+++ sorter/src/index.ts
@@ -1,1 +1,23 @@
-console.log('woah, crazy!');
+const array = [200, 1, 5, 2, 23, 322, 3, 5, 76, -1, 6, 64];
+
+// my bubble sorting algorithm
+function sort(array: number[]): number[] {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      const val = array[j];
+      const nextVal = array[j + 1];
+      if (val > nextVal) {
+        array[j] = nextVal;
+        array[j + 1] = val;
+      }
+    }
+  }
+  return array;
+}
+
+console.log(sort(array));
+
+class Sorter {
+  constructor(public collection: number[]) {}
+  sort(): void {}
+}
