9e7a452def61706a2df04875ac6a125eef5d9e69
Jordi M. Zambrano <nickschoey@gmail.com>
2020-02-28 10:39:50 +0100

    lesson 87

---
+++ sorter/src/LinkedList.ts
@@ -0,0 +1,76 @@
+class Node {
+  next: Node | null = null;
+  constructor(public value: number) {}
+}
+
+export class LinkedList {
+  head: Node | null = null;
+  add(value: number) {
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+    let tail = this.head;
+    while (tail.next) {
+      tail = tail.next;
+    }
+    tail.next = node;
+  }
+  get length(): number {
+    if (!this.head) {
+      return 0;
+    }
+    let count = 1;
+    let pointer = this.head;
+    while (pointer.next) {
+      length++;
+      pointer = pointer.next;
+    }
+    return count;
+  }
+
+  at(index: number): Node {
+    if (!this.head) {
+      throw new Error('Index out of bounds');
+    }
+    let counter = 0;
+    let node: Node | null = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+      counter++;
+      node = node.next;
+    }
+    throw new Error('Index out of bounds');
+  }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    if (!this.head) {
+      throw new Error('List is empty');
+    }
+
+    return this.at(leftIndex).value > this.at(rightIndex).value;
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    const leftNode = this.at(leftIndex);
+    const rightNode = this.at(rightIndex);
+
+    const leftValue = leftNode.value;
+    leftNode.value = rightNode.value;
+    rightNode.value = leftValue;
+  }
+
+  print(): void {
+    if (!this.head) {
+      return;
+    }
+    let node: Node | null = this.head;
+    while (node) {
+      console.log(node.value);
+      node = node.next;
+    }
+  }
+}
