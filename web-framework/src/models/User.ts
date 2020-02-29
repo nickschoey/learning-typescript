a3489e5dd417f757f90ad483dc8e67934471195b
Jordi M. Zambrano <nickschoey@gmail.com>
2020-02-29 16:07:29 +0100

    lesson 142

--- web-framework/src/models/User.ts
+++ web-framework/src/models/User.ts
@@ -1,11 +1,13 @@
 interface UserProps {
-  name: string;
-  age: number;
+  name?: string;
+  age?: number;
 }
 export class User {
   constructor(private data: UserProps) {}
   get(propName: string): string | number {
     return this.data[propName];
   }
-  set() {}
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
 }
