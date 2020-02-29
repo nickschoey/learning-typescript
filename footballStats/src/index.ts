e60c60f3701ef161613385c1945d8d1831704c76
Jordi M. Zambrano <nickschoey@gmail.com>
2020-02-29 08:47:01 +0100

    lesson 120

--- footballStats/src/index.ts
+++ footballStats/src/index.ts
@@ -1,12 +1,15 @@
 import { MatchResult } from './MatchResults';
 import { MatchReader } from './MatchReader';
+import { CsvFileReader } from './CsvFileReader';
 
-const reader = new MatchReader('football.csv');
-reader.read();
-
+// Create an object that satisfies the DataReader Interface
+const csvFileReader = new CsvFileReader('football.csv');
+// Create an instance of MatchReader and pass something that satisifies the DataReader interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 let manUWins = 0;
 
-for (const match of reader.data) {
+for (const match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
@@ -15,4 +18,3 @@ for (const match of reader.data) {
 }
 
 console.log(manUWins);
-console.log(reader.data);
