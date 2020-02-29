c8fdd477fa119c233ecdf7394c7a6460be6bba53
Jordi M. Zambrano <nickschoey@gmail.com>
2020-02-29 10:38:48 +0100

    lesson 130

--- footballStats/src/index.ts
+++ footballStats/src/index.ts
@@ -1,13 +1,11 @@
 import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
-import { ConsoleReport, HtmlReport } from './Reporters';
-import { WinsAnalysis } from './Analyzers';
 import { Summary } from './Summary';
 
 const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+const summary = Summary.winsAnalysisWithConsoleReport('Liverpool');
 
 summary.buildAndPrintReport(matchReader.matches);
