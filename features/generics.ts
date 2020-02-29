ca3cfb57b2b52ea58a21a4cfd7a7dcab228c320b
Jordi M. Zambrano <nickschoey@gmail.com>
2020-02-29 13:58:41 +0100

    lesson 136

--- features/generics.ts
+++ features/generics.ts
@@ -22,3 +22,52 @@ new ArrayOfAnything<string>(['a', 'b', 'c']);
 new ArrayOfAnything<number>([1, 2, 3, 4]);
 
 const arr = new ArrayOfAnything([1, 2, 3, 4]);
+
+// Example of generics with Functions
+function printStrings(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printNumbers(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+// Generic Constraints
+
+class Car {
+  print() {
+    console.log('I am a car');
+  }
+}
+
+class House {
+  print() {
+    console.log('I am a car');
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+// Error! Thanks Obama! (Interface)
+printHousesOrCars([1, 2, 3, 4]);
+// Coolio
+printHousesOrCars([new Car(), new Car()]);
+printHousesOrCars([new House(), new House()]);
