70411fca6cade1d74d8b07db7735d27cdaa25d1d
Jordi M. Zambrano <nickschoey@gmail.com>
2020-02-29 08:43:04 +0100

    lesson 119

--- footballStats/src/MatchReader.ts
+++ footballStats/src/MatchReader.ts
@@ -1,8 +1,30 @@
+import { dateStringToDate } from './utils';
+import { MatchResult } from './MatchResults';
+
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
 interface DataReader {
   read(): void;
   data: string[][];
 }
 
-class MatchReader {
+export class MatchReader {
+  matches: MatchData[] = [];
   constructor(public reader: DataReader) {}
+  load(): void {
+    this.reader.read();
+    this.matches = this.reader.data.map(
+      (match: string[]): MatchData => {
+        return [
+          dateStringToDate(match[0]),
+          match[1],
+          match[2],
+          parseInt(match[3]),
+          parseInt(match[4]),
+          match[5] as MatchResult,
+          match[6]
+        ];
+      }
+    );
+  }
 }
