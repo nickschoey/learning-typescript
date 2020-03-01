2430b6c9a6ba53c5941c3755d3d04eaf59fa3ee0
Jordi M. Zambrano <nickschoey@gmail.com>
2020-03-01 07:55:56 +0100

    lesson 153

--- web-framework/src/models/User.ts
+++ web-framework/src/models/User.ts
@@ -6,11 +6,7 @@ interface UserProps {
   age?: number;
 }
 
-type Callback = () => void;
-
 export class User {
-  events: { [key: string]: Callback[] } = {};
-
   constructor(private data: UserProps) {}
 
   get(propName: string): string | number {
@@ -21,21 +17,6 @@ export class User {
     Object.assign(this.data, update);
   }
 
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  }
-
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-    if (!handlers || handlers.length === 0) {
-      return;
-    }
-    handlers.forEach(callback => {
-      callback();
-    });
-  }
   fetch(): void {
     const url = 'http://localhost:3000/users';
     const id = this.get('id');
