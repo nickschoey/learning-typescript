7d61d9030ea17bf98ffa6482fc93c0fc8877cf27
Jordi M. Zambrano <nickschoey@gmail.com>
2020-03-01 10:20:04 +0100

    lesson 170

--- web-framework/src/index.ts
+++ web-framework/src/index.ts
@@ -2,6 +2,19 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Gio', age: 22 });
 
-user.on('change', () => {
-  console.log('hey!');
-});
+console.log(user.get('name'));
+
+// reminder on how 'this' works in JS
+
+const colors = {
+  color: 'red',
+  printColor() {
+    console.log(this.color);
+  }
+};
+
+colors.printColor(); //red
+
+const printColor = colors.printColor;
+
+printColor(); // error: color is undefined
--- web-framework/src/models/Sync.ts
+++ web-framework/src/models/Sync.ts
@@ -6,16 +6,17 @@ interface HasId {
 
 export class Sync<T extends HasId> {
   constructor(public rootUrl: string) {}
-  fetch(id: number): AxiosPromise {
-    return axios.get(`${this.rootUrl}/${id}`);
-  }
 
-  save(data: T): AxiosPromise {
+  fetch = (id: number): AxiosPromise => {
+    return axios.get(`${this.rootUrl}/${id}`);
+  };
+
+  save = (data: T): AxiosPromise => {
     const { id } = data;
     if (id) {
       return axios.put(`${this.rootUrl}/${id}`, data);
     } else {
       return axios.post(this.rootUrl, data);
     }
-  }
+  };
 }
