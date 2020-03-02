cfd31df236a231c2d6a2785bfa1caddbfe992c09
Jordi M. Zambrano <nickschoey@gmail.com>
2020-03-02 15:38:25 +0100

    Lesson 207

--- web-framework/src/index.ts
+++ web-framework/src/index.ts
@@ -1,13 +1,18 @@
-import { UserEdit } from './views/UserEdit';
-import { User } from './models/User';
+import { UserList } from './views/UserList';
+import { Collection } from './models/Collection';
+import { UserProps, User } from './models/User';
 
-const user = User.buildUser({ name: 'Jordi', age: 36 });
-const rootElement = document.getElementById('root');
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
 
-if (rootElement) {
-  const userEdit = new UserEdit(rootElement, user);
-  userEdit.render();
-  console.log(userEdit);
-} else {
-  throw new Error('Root Element not found');
-}
+users.on('change', () => {
+  const root = document.getElementById('root');
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+users.fetch();
