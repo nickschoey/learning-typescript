4246b2a91cb74961a7be0617398cc249e31f7f72
Jordi M. Zambrano <nickschoey@gmail.com>
2020-03-02 11:10:44 +0100

    lesson 190

--- web-framework/src/views/UserForm.ts
+++ web-framework/src/views/UserForm.ts
@@ -20,9 +20,20 @@ export class UserForm {
     `;
   }
 
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+    for (const key in eventsMap) {
+      const [eventName, selector] = key.split(':');
+      fragment.querySelectorAll(selector).forEach(element => {
+        element.addEventListener(eventName, eventsMap[key]);
+      });
+    }
+  }
+
   render(): void {
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
+    this.bindEvents(templateElement.content);
 
     this.parent.append(templateElement.content);
   }
