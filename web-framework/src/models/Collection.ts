86b77e5d9ace4b177dfdd9d3d46c2bfe09a1bb66
Jordi M. Zambrano <nickschoey@gmail.com>
2020-03-02 08:32:52 +0100

    lesson 181

---
+++ web-framework/src/models/Collection.ts
@@ -0,0 +1,15 @@
+import { User } from './../../../maps/src/User';
+import { Eventing } from './Eventing';
+
+export class Collection {
+  models: User[] = [];
+  events: Eventing = new Eventing();
+
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+}
