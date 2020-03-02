b431bd07a62a7bcbe8f96af165969dc98b7f2297
Jordi M. Zambrano <nickschoey@gmail.com>
2020-03-02 11:30:42 +0100

    lesson 192

--- web-framework/src/views/UserForm.ts
+++ web-framework/src/views/UserForm.ts
@@ -5,12 +5,16 @@ export class UserForm {
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      'click:button': this.onButtonClicked
+      'click:.set-age': this.onSetAgeClick
     };
   }
 
   onButtonClicked(): void {
     console.log('Hi there!');
+  }
+
+  onSetAgeClick(): void {
+    console.log('Stuff happening');
   }
   template(): string {
     return `
@@ -18,7 +22,8 @@ export class UserForm {
       <h1>User Form</h1>
       <div>User Name: ${this.model.get('name')}</div>
       <div>User Age: ${this.model.get('age')}</div>
-      <input></input>
+      <input/>
+      <button class="set-age">Set Random Age</button>
       <button>Click Me!</button>
     </div>
     `;
