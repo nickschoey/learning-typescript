427594c18b3bb0365d970b32bd2fc354c52c104c
Jordi M. Zambrano <nickschoey@gmail.com>
2020-03-03 10:28:50 +0100

    lesson 214

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,10 @@
 import express, { Response, Request } from 'express';
 import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
+
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
 
 app.listen(3000, () => {
