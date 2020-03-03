e0d954becd38cd3c0e063f184552f6443fccfed3
Jordi M. Zambrano <nickschoey@gmail.com>
2020-03-03 08:51:54 +0100

    lesson 213

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,8 @@
 import express, { Response, Request } from 'express';
-
+import { router } from './routes/loginRoutes';
 const app = express();
 
-app.get('/', (req: Request, res: Response): void => {
-  res.send(`
-  <div>
-    <h1>This works</h1>
-  </div>
-  `);
-});
+app.use(router);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
