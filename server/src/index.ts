61376fcebc0b897ce523c76c11b4f02ebd6a1be6
Jordi M. Zambrano <nickschoey@gmail.com>
2020-03-03 08:47:50 +0100

    lesson 212

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,15 @@
-console.log('hi there');
+import express, { Response, Request } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response): void => {
+  res.send(`
+  <div>
+    <h1>This works</h1>
+  </div>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
