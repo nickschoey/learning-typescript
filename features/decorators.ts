fd6df9664475e4aaa6ac314c2646ee06b22c2acf
Jordi M. Zambrano <nickschoey@gmail.com>
2020-03-04 11:43:33 +0100

    lesson 230

--- features/decorators.ts
+++ features/decorators.ts
@@ -1,14 +1,36 @@
+@classDecorator
 class Boat {
+  @testDecorator
   color: string = 'red';
 
   @logError('An error was thrown by pilot')
-  pilot(): void {
+  pilot(@parameterDecorator speed: string): void {
+    if (speed === 'fast') {
+      console.log('swish');
+    } else {
+      console.log('vrum');
+    }
     throw new Error();
   }
   get formattedColor(): string {
     return `The color is ${this.color}`;
   }
 }
+
+function classDecorator(constructor: typeof Boat) {
+  console.log(constructor);
+}
+
+function parameterDecorator(target: any, key: string, index: number) {
+  console.log(key, index);
+}
+
+function testDecorator(target: any, key: string) {
+  console.log(target);
+  console.log(key);
+  console.log(target.color);
+}
+
 function logError(errorMessage: string) {
   return function(target: any, key: string, desc: PropertyDescriptor): void {
     const method = desc.value;
@@ -21,5 +43,3 @@ function logError(errorMessage: string) {
     };
   };
 }
-
-new Boat().pilot();
