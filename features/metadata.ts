4f5440ed83c8b5f6d643b7641b8009e01d50c5de
Jordi M. Zambrano <nickschoey@gmail.com>
2020-03-07 12:29:40 +0100

    lesson 237

--- features/metadata.ts
+++ features/metadata.ts
@@ -6,3 +6,31 @@ const plane = {
 
 Reflect.defineMetadata('note', 'hi there', plane);
 const note = Reflect.getMetadata('note', plane);
+
+// In classes
+@printMetadata
+class Plane {
+  color: string = 'white';
+
+  @markFunction('hellow')
+  fly(): void {
+    console.log('fuaaaa');
+  }
+}
+
+function markFunction(secretInfo: string) {
+  return function(target: Plane, key: string) {
+    Reflect.defineMetadata('secret', secretInfo, target, key);
+  };
+}
+
+function printMetadata(target: typeof Plane) {
+  for (const key in target.prototype) {
+    const secret = Reflect.getMetadata('secret', target.prototype, key);
+    console.log(secret);
+  }
+}
+
+const secret = Reflect.getMetadata('secret', Plane.prototype, 'fly');
+
+console.log(secret);
